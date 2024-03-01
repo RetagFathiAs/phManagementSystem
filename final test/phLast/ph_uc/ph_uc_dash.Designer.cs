f9ceb15c553164936674b9be465c5fb9aedf6324
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-03-01 03:35:00 +0200

    Add files via upload

---
+++ final test/phLast/ph_uc/ph_uc_dash.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ph.ph_uc
+{
+  partial class ph_uc_dash
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.panel3 = new System.Windows.Forms.Panel();
+      this.panel2 = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.button1 = new System.Windows.Forms.Button();
+      this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.MistyRose;
+      this.panel1.Controls.Add(this.panel3);
+      this.panel1.Controls.Add(this.panel2);
+      this.panel1.Controls.Add(this.pictureBox1);
+      this.panel1.Location = new System.Drawing.Point(23, 27);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(430, 576);
+      this.panel1.TabIndex = 0;
+      // 
+      // panel3
+      // 
+      this.panel3.BackColor = System.Drawing.Color.DarkCyan;
+      this.panel3.Location = new System.Drawing.Point(120, 453);
+      this.panel3.Name = "panel3";
+      this.panel3.Size = new System.Drawing.Size(314, 10);
+      this.panel3.TabIndex = 2;
+      // 
+      // panel2
+      // 
+      this.panel2.BackColor = System.Drawing.Color.White;
+      this.panel2.Location = new System.Drawing.Point(120, 40);
+      this.panel2.Name = "panel2";
+      this.panel2.Size = new System.Drawing.Size(310, 10);
+      this.panel2.TabIndex = 1;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = global::ph.Properties.Resources.drugs;
+      this.pictureBox1.Location = new System.Drawing.Point(183, 127);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(247, 289);
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // chart1
+      // 
+      this.chart1.BackColor = System.Drawing.Color.RosyBrown;
+      chartArea1.Name = "ChartArea1";
+      this.chart1.ChartAreas.Add(chartArea1);
+      legend1.Name = "Expired Medicine";
+      this.chart1.Legends.Add(legend1);
+      this.chart1.Location = new System.Drawing.Point(459, 27);
+      this.chart1.Name = "chart1";
+      series1.ChartArea = "ChartArea1";
+      series1.Color = System.Drawing.Color.Firebrick;
+      series1.Legend = "Expired Medicine";
+      series1.Name = "Expired Medicine";
+      series2.ChartArea = "ChartArea1";
+      series2.Color = System.Drawing.Color.DarkCyan;
+      series2.Legend = "Expired Medicine";
+      series2.Name = "Valid Medicine";
+      this.chart1.Series.Add(series1);
+      this.chart1.Series.Add(series2);
+      this.chart1.Size = new System.Drawing.Size(513, 576);
+      this.chart1.TabIndex = 1;
+      this.chart1.Text = "chart1";
+      title1.Name = "title1";
+      this.chart1.Titles.Add(title1);
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.PowderBlue;
+      this.button1.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.Location = new System.Drawing.Point(832, 530);
+      this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(129, 55);
+      this.button1.TabIndex = 7;
+      this.button1.Text = "REFRESH";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // guna2Elipse1
+      // 
+      this.guna2Elipse1.TargetControl = this;
+      // 
+      // ph_uc_dash
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.White;
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.chart1);
+      this.Controls.Add(this.panel1);
+      this.Name = "ph_uc_dash";
+      this.Size = new System.Drawing.Size(1047, 678);
+      this.Load += new System.EventHandler(this.ph_uc_dash_Load);
+      this.panel1.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Panel panel2;
+    private System.Windows.Forms.Panel panel3;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    private System.Windows.Forms.Button button1;
+    private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+  }
+}
---
+++ final test/phLast/uc/uc_adduser.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace ph.uc
+{
+    partial class uc_adduser
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.components = new System.ComponentModel.Container();
+      this.DashBoard = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.Username = new System.Windows.Forms.Label();
+      this.label7 = new System.Windows.Forms.Label();
+      this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+      this.rolcombo = new System.Windows.Forms.ComboBox();
+      this.nametxt = new System.Windows.Forms.TextBox();
+      this.mobiletxt = new System.Windows.Forms.TextBox();
+      this.emailtxt = new System.Windows.Forms.TextBox();
+      this.usertxt = new System.Windows.Forms.TextBox();
+      this.passtxt = new System.Windows.Forms.TextBox();
+      this.birthcombo = new System.Windows.Forms.ComboBox();
+      this.signbtn = new System.Windows.Forms.Button();
+      this.resetbtn = new System.Windows.Forms.Button();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // DashBoard
+      // 
+      this.DashBoard.AutoSize = true;
+      this.DashBoard.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.DashBoard.Location = new System.Drawing.Point(34, 23);
+      this.DashBoard.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.DashBoard.Name = "DashBoard";
+      this.DashBoard.Size = new System.Drawing.Size(164, 45);
+      this.DashBoard.TabIndex = 1;
+      this.DashBoard.Text = "Add User";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(61, 114);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(49, 26);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "Role";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(61, 204);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(62, 26);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Name";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label3.Location = new System.Drawing.Point(61, 299);
+      this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(53, 26);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "DOB";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label4.Location = new System.Drawing.Point(61, 406);
+      this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(69, 26);
+      this.label4.TabIndex = 5;
+      this.label4.Text = "Mobile";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label5.Location = new System.Drawing.Point(403, 114);
+      this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(58, 26);
+      this.label5.TabIndex = 6;
+      this.label5.Text = "Email";
+      // 
+      // Username
+      // 
+      this.Username.AutoSize = true;
+      this.Username.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Username.Location = new System.Drawing.Point(403, 204);
+      this.Username.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.Username.Name = "Username";
+      this.Username.Size = new System.Drawing.Size(99, 26);
+      this.Username.TabIndex = 7;
+      this.Username.Text = "Username";
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label7.Location = new System.Drawing.Point(403, 299);
+      this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(94, 26);
+      this.label7.TabIndex = 8;
+      this.label7.Text = "Password";
+      // 
+      // guna2Elipse1
+      // 
+      this.guna2Elipse1.TargetControl = this;
+      // 
+      // rolcombo
+      // 
+      this.rolcombo.Font = new System.Drawing.Font("Segoe UI", 12F);
+      this.rolcombo.FormattingEnabled = true;
+      this.rolcombo.Items.AddRange(new object[] {
+            "Pharmacist.",
+            "Adminstrator."});
+      this.rolcombo.Location = new System.Drawing.Point(66, 143);
+      this.rolcombo.Name = "rolcombo";
+      this.rolcombo.Size = new System.Drawing.Size(300, 29);
+      this.rolcombo.TabIndex = 9;
+      // 
+      // nametxt
+      // 
+      this.nametxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.nametxt.Location = new System.Drawing.Point(66, 233);
+      this.nametxt.Name = "nametxt";
+      this.nametxt.Size = new System.Drawing.Size(300, 29);
+      this.nametxt.TabIndex = 10;
+      // 
+      // mobiletxt
+      // 
+      this.mobiletxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.mobiletxt.Location = new System.Drawing.Point(66, 444);
+      this.mobiletxt.Name = "mobiletxt";
+      this.mobiletxt.Size = new System.Drawing.Size(300, 29);
+      this.mobiletxt.TabIndex = 11;
+      // 
+      // emailtxt
+      // 
+      this.emailtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.emailtxt.Location = new System.Drawing.Point(408, 143);
+      this.emailtxt.Name = "emailtxt";
+      this.emailtxt.Size = new System.Drawing.Size(300, 29);
+      this.emailtxt.TabIndex = 12;
+      // 
+      // usertxt
+      // 
+      this.usertxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.usertxt.Location = new System.Drawing.Point(408, 233);
+      this.usertxt.Name = "usertxt";
+      this.usertxt.Size = new System.Drawing.Size(300, 29);
+      this.usertxt.TabIndex = 13;
+      this.usertxt.TextChanged += new System.EventHandler(this.usertxt_TextChanged);
+      // 
+      // passtxt
+      // 
+      this.passtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.passtxt.Location = new System.Drawing.Point(408, 338);
+      this.passtxt.Name = "passtxt";
+      this.passtxt.PasswordChar = '#';
+      this.passtxt.Size = new System.Drawing.Size(300, 29);
+      this.passtxt.TabIndex = 14;
+      // 
+      // birthcombo
+      // 
+      this.birthcombo.Font = new System.Drawing.Font("Segoe UI", 12F);
+      this.birthcombo.FormattingEnabled = true;
+      this.birthcombo.Location = new System.Drawing.Point(66, 338);
+      this.birthcombo.Name = "birthcombo";
+      this.birthcombo.Size = new System.Drawing.Size(300, 29);
+      this.birthcombo.TabIndex = 15;
+      // 
+      // signbtn
+      // 
+      this.signbtn.BackColor = System.Drawing.Color.Bisque;
+      this.signbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+      this.signbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+      this.signbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.signbtn.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.signbtn.Location = new System.Drawing.Point(427, 392);
+      this.signbtn.Name = "signbtn";
+      this.signbtn.Size = new System.Drawing.Size(102, 40);
+      this.signbtn.TabIndex = 16;
+      this.signbtn.Text = "Sign In";
+      this.signbtn.UseVisualStyleBackColor = false;
+      this.signbtn.Click += new System.EventHandler(this.signbtn_Click);
+      // 
+      // resetbtn
+      // 
+      this.resetbtn.BackColor = System.Drawing.Color.Bisque;
+      this.resetbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+      this.resetbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      this.resetbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.resetbtn.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.resetbtn.Location = new System.Drawing.Point(560, 392);
+      this.resetbtn.Name = "resetbtn";
+      this.resetbtn.Size = new System.Drawing.Size(97, 40);
+      this.resetbtn.TabIndex = 17;
+      this.resetbtn.Text = "Reset";
+      this.resetbtn.UseVisualStyleBackColor = false;
+      this.resetbtn.Click += new System.EventHandler(this.resetbtn_Click);
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.DarkOrange;
+      this.panel1.Location = new System.Drawing.Point(381, 40);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(5, 368);
+      this.panel1.TabIndex = 18;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(711, 227);
+      this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(45, 32);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pictureBox1.TabIndex = 19;
+      this.pictureBox1.TabStop = false;
+      // 
+      // uc_adduser
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.White;
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.panel1);
+      this.Controls.Add(this.resetbtn);
+      this.Controls.Add(this.signbtn);
+      this.Controls.Add(this.birthcombo);
+      this.Controls.Add(this.passtxt);
+      this.Controls.Add(this.usertxt);
+      this.Controls.Add(this.emailtxt);
+      this.Controls.Add(this.mobiletxt);
+      this.Controls.Add(this.nametxt);
+      this.Controls.Add(this.rolcombo);
+      this.Controls.Add(this.label7);
+      this.Controls.Add(this.Username);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.DashBoard);
+      this.Name = "uc_adduser";
+      this.Size = new System.Drawing.Size(771, 494);
+      this.Load += new System.EventHandler(this.uc_adduser_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DashBoard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label label7;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private System.Windows.Forms.ComboBox rolcombo;
+        private System.Windows.Forms.ComboBox birthcombo;
+        private System.Windows.Forms.TextBox passtxt;
+        private System.Windows.Forms.TextBox usertxt;
+        private System.Windows.Forms.TextBox emailtxt;
+        private System.Windows.Forms.TextBox mobiletxt;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.Button resetbtn;
+        private System.Windows.Forms.Button signbtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
