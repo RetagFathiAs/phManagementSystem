2a25b198a10fea1d6f83a1ebe07319e26a3213d4
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-03-06 13:41:02 +0200

    alx

---
+++ final test/phLast/ph_uc/uc_inventory.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ph.ph_uc
+{
+  partial class uc_inventory
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.View = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.DarkCyan;
+      this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.ForeColor = System.Drawing.Color.White;
+      this.button1.Location = new System.Drawing.Point(897, 18);
+      this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(113, 46);
+      this.button1.TabIndex = 14;
+      this.button1.Text = "REFRESH";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.ForeColor = System.Drawing.Color.CadetBlue;
+      this.textBox1.Location = new System.Drawing.Point(165, 356);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(289, 22);
+      this.textBox1.TabIndex = 13;
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.BackgroundColor = System.Drawing.Color.PowderBlue;
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(165, 386);
+      this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.RowHeadersWidth = 51;
+      this.dataGridView1.Size = new System.Drawing.Size(733, 218);
+      this.dataGridView1.TabIndex = 12;
+      // 
+      // View
+      // 
+      this.View.AutoSize = true;
+      this.View.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.View.ForeColor = System.Drawing.Color.CadetBlue;
+      this.View.Location = new System.Drawing.Point(84, 94);
+      this.View.Name = "View";
+      this.View.Size = new System.Drawing.Size(112, 56);
+      this.View.TabIndex = 11;
+      this.View.Text = "View";
+      // 
+      // uc_inventory
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackgroundImage = global::ph.Properties.Resources.Screenshot_2024_03_05_210337;
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.dataGridView1);
+      this.Controls.Add(this.View);
+      this.DoubleBuffered = true;
+      this.Name = "uc_inventory";
+      this.Size = new System.Drawing.Size(1028, 608);
+      this.Load += new System.EventHandler(this.uc_inventory_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.DataGridView dataGridView1;
+    private System.Windows.Forms.Label View;
+  }
+}
